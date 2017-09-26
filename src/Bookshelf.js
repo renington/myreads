d9e5234e85124f1eaf8768a939d22eda1ecdfda4
Renington Neri <renington.neri@gmail.com>
2017-09-25 22:32:54 -0300

    Initial commit with features: Book Lists and BookShelves.

---
+++ src/Bookshelf.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import ListBooks from './ListBooks'
+
+class Bookshelf extends Component {
+    static propTypes = {
+        books: PropTypes.array.isRequired,
+        updateShelf: PropTypes.func.isRequired
+    }
+
+    render() {
+        const shelves = [
+            ["currentlyReading","Currently Reading"], 
+            ["wantToRead","Want To Read"],
+            ["read", "Read"]]
+        return (
+            
+            <div className="list-books-content">
+                {shelves.map((shelf, key) => {
+                    return (
+                        <div key={key} className="bookshelf">
+                            <h2 className="bookshelf-title">{shelf[1]}</h2>
+                            <div className="bookshelf-books">
+                                <ListBooks
+                                    books={this.props.books.filter(book => book.shelf === shelf[0])}
+                                    updateShelf={this.props.updateShelf}
+                                />
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }
+}
+
+export default Bookshelf
---
+++ src/ListBooks.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+
+class ListBooks extends Component {
+    static propTypes = {
+        books: PropTypes.array.isRequired,
+        updateShelf: PropTypes.func.isRequired
+    }
+
+    render(){
+        return (
+            <ol className="books-grid">
+                {this.props.books.map((book, key) => (
+                <li key={key}>
+                    <div className="book" >
+                        <div className="book-top">
+                        <div className='book-cover' style={{
+                            width: 128, 
+                            height: 193,
+                            backgroundImage: `url(${book.imageLinks.smallThumbnail})`
+                        }}/>
+                            <div className="book-shelf-changer">
+                                <select value={book.shelf} onChange={(event)=>{this.props.updateShelf(book, event.target.value)}}>
+                                    <option value="none" disabled>
+                                        Move to...
+                                    </option>
+                                    <option value="currentlyReading">Currently Reading</option>
+                                    <option value="wantToRead">Want to Read</option>
+                                    <option value="read">Read</option>
+                                    <option value="none">None</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className="book-title">{book.title}</div>
+                        <div className="book-authors">{book.authors}</div>
+                    </div>
+                </li>
+                ))}
+            </ol>
+        )
+    }
+}
+
+export default ListBooks
