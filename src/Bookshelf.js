704f6179bf54e0ced8212b1dd597cfa90b5f9432
Renington Neri <renington.neri@gmail.com>
2017-09-30 10:57:36 -0300

    [UPDATE] Refactoring Bookshelf. Convert components to stateless functional components

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,37 +1,35 @@
-import React, {Component} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import ListBooks from './ListBooks'
 
-class Bookshelf extends Component {
-    static propTypes = {
-        books: PropTypes.array.isRequired,
-        updateShelf: PropTypes.func.isRequired
-    }
+const Bookshelf = (props) => {
+    const shelves = [
+        ["currentlyReading","Currently Reading"], 
+        ["wantToRead","Want To Read"],
+        ["read", "Read"]]
 
-    render() {
-        const shelves = [
-            ["currentlyReading","Currently Reading"], 
-            ["wantToRead","Want To Read"],
-            ["read", "Read"]]
-            
-        return (
-            <div className="list-books-content">
-                {shelves.map((shelf, key) => {
-                    return (
-                        <div key={key} className="bookshelf">
-                            <h2 className="bookshelf-title">{shelf[1]}</h2>
-                            <div className="bookshelf-books">
-                                <ListBooks
-                                    books={this.props.books.filter(book => book.shelf === shelf[0])}
-                                    updateShelf={this.props.updateShelf}
-                                />
-                            </div>
+    return (
+        <div className="list-books-content">
+            {shelves.map((shelf, key) => {
+                return (
+                    <div key={key} className="bookshelf">
+                        <h2 className="bookshelf-title">{shelf[1]}</h2>
+                        <div className="bookshelf-books">
+                            <ListBooks
+                                books={props.books.filter(book => book.shelf === shelf[0])}
+                                updateShelf={props.updateShelf}
+                            />
                         </div>
-                    )
-                })}
-            </div>
-        )
-    }
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+Bookshelf.propTypes = {
+    books: PropTypes.array.isRequired,
+    updateShelf: PropTypes.func.isRequired
 }
 
 export default Bookshelf
