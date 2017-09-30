0535aefff602c9ea8ccb0b74ad12ffa4f3c995af
Renington Neri <renington.neri@gmail.com>
2017-09-29 21:53:44 -0300

    [UPDATE] Refactoring. Convert components to stateless functional components

--- src/Search.js
+++ src/Search.js
@@ -1,27 +1,26 @@
-import React, { Component } from 'react'
+import React from 'react'
 import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types'
 
-class Search extends Component {
-    static propTypes = {
-        search: PropTypes.func.isRequired
-    }
-
-    render(){
-        return (
-            <div className="search-books-bar">
-                <Link className="close-search" to="/">Close</Link>
-                <div className="search-books-input-wrapper">
-                <input 
-                    className='search-books'
-                    type='text'
-                    placeholder='Search by title or author'
-                    onChange={(event) => this.props.search(event.target.value)}
-                />
-                </div>
+// Functional Stateless Components
+const Search = (props) => {
+    return (
+        <div className="search-books-bar">
+            <Link className="close-search" to="/">Close</Link>
+            <div className="search-books-input-wrapper">
+            <input 
+                className='search-books'
+                type='text'
+                placeholder='Search by title or author'
+                onChange={(event) => props.search(event.target.value)}
+            />
             </div>
-        )
-    }
+        </div>
+    )
+}
+
+Search.propTypes = {
+    search: PropTypes.func.isRequired
 }
 
 export default Search
