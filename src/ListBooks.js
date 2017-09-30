45bd1612a4d9435e9a4d45f8834f69ce0f21463b
Renington Neri <renington.neri@gmail.com>
2017-09-30 11:01:04 -0300

    [UPDATE] Refactoring ListBooks. Convert components to stateless functional components

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,42 +1,41 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 
-class ListBooks extends Component {
-    static propTypes = {
-        books: PropTypes.array.isRequired,
-        updateShelf: PropTypes.func.isRequired
-    }
 
-    render(){
-        return (
-            <ol className="books-grid">
-                {this.props.books.map((book, key) => (
-                <li key={key}>
-                    <div className="book" >
-                        <div className="book-top">
-                        <div className='book-cover' style={{
-                            width: 128, 
-                            height: 193,
-                            backgroundImage: `url(${book.imageLinks.smallThumbnail})`
-                        }}/>
-                            <div className="book-shelf-changer">
-                                <select value={book.shelf ? book.shelf : 'none'} onChange={(event)=>{this.props.updateShelf(book, event.target.value)}}>
-                                    <option value="" disabled>Move to...</option>
-                                    <option value="currentlyReading">Currently Reading</option>
-                                    <option value="wantToRead">Want to Read</option>
-                                    <option value="read">Read</option>
-                                    <option value="none">None</option>
-                                </select>
-                            </div>
+const ListBooks = (props) => {
+    return (
+        <ol className="books-grid">
+            {props.books.map((book, key) => (
+            <li key={key}>
+                <div className="book" >
+                    <div className="book-top">
+                    <div className='book-cover' style={{
+                        width: 128, 
+                        height: 193,
+                        backgroundImage: `url(${book.imageLinks.smallThumbnail})`
+                    }}/>
+                        <div className="book-shelf-changer">
+                            <select value={book.shelf ? book.shelf : 'none'} onChange={(event)=>{props.updateShelf(book, event.target.value)}}>
+                                <option value="" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                            </select>
                         </div>
-                        <div className="book-title">{book.title}</div>
-                        <div className="book-authors">{(book.authors!==undefined && book.authors.length>0)?book.authors.join(', '):''}</div>
                     </div>
-                </li>
-                ))}
-            </ol>
-        )
-    }
+                    <div className="book-title">{book.title}</div>
+                    <div className="book-authors">{(book.authors!==undefined && book.authors.length>0)?book.authors.join(', '):''}</div>
+                </div>
+            </li>
+            ))}
+        </ol>
+    )
+}
+
+ListBooks.propTypes = {
+    books: PropTypes.array.isRequired,
+    updateShelf: PropTypes.func.isRequired
 }
 
 export default ListBooks
