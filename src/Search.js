b7901e4a7dd3e5916c3edda438f94d49c9f0f30f
Renington Neri <renington.neri@gmail.com>
2017-09-29 23:10:40 -0300

    [ADD] Add new lib about Debounce input search

--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types'
+import { Debounce } from "react-throttle";
 
 // Functional Stateless Components
 const Search = (props) => {
@@ -8,12 +9,14 @@ const Search = (props) => {
         <div className="search-books-bar">
             <Link className="close-search" to="/">Close</Link>
             <div className="search-books-input-wrapper">
-            <input 
-                className='search-books'
-                type='text'
-                placeholder='Search by title or author'
-                onChange={(event) => props.search(event.target.value)}
-            />
+            <Debounce time="200" handler="onChange">
+                <input 
+                    className='search-books'
+                    type='text'
+                    placeholder='Search by title or author'
+                    onChange={(event) => props.search(event.target.value)}
+                />
+            </Debounce>
             </div>
         </div>
     )
